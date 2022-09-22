ca60e688260e49c9b1ff54ff63f13663ca5062c7
merlotliu <meetlouisliu@163.com>
2022-09-22 16:07:45 +0800

    update

--- coding/Graph/code07_arrange_journey.hpp
+++ coding/Graph/code07_arrange_journey.hpp
@@ -19,12 +19,13 @@ public:
 	std::vector<Edge*> edges;
 };
 
-class Edge{
+class Edge {
 public:
 	Edge(Vertex* f, Vertex* t) : from(f), to(t) {}
 public:
 	Vertex* from;
 	Vertex* to;
+	string weight;
 };
 
 class Graph {
@@ -50,6 +51,7 @@ public:
 			Vertex* from = graph->vertex_map[ticket[0]];
 			Vertex* to = graph->vertex_map[ticket[1]];
 			Edge *edge = new Edge(from, to);
+			edge->weight = ticket[1];
 			graph->edges.insert(edge);
 			from->nexts.push_back(to);
 			from->edges.push_back(edge);
@@ -76,44 +78,69 @@ public:
 			}
 		}
 	}
-
-	void dfs(Graph *graph, Vertex *cur, int count) {
-		if (count == graph->edges.size()) {
-			if (res.empty()) {
-				res = picked; 
-				return;
-			}
-			int i = 0;
-			while (i < picked.size() && picked[i] == res[i]) i++;
-			if (i < picked.size() && picked[i] < res[i]) {
-				res = picked;
-			}
+	
+	void minDictJourey() {
+		if (res.empty()) {
+			res = picked;
 			return;
 		}
+		int i = 0;
+		while (i < picked.size() && picked[i] == res[i]) i++;
+		if (i < picked.size() && picked[i] < res[i]) {
+			res = picked;
+			return;
+		}
+		return;
+	}
+
+	vector<string> dfs(Graph *graph, Vertex *cur) {
 		unordered_set<Edge*> memo;
-		for (auto edge : cur->edges) {
+		while (memo.size() != graph->edges.size()) {
+			Edge *min_edge = nullptr;
+			for(auto edge : cur->edges) {
+				if (memo.find(edge) == memo.end()) {
+					if (min_edge == nullptr || min_edge->weight > edge->weight) {
+						min_edge = edge;
+					}
+				}
+			}
+			if (min_edge) {
+				memo.insert(min_edge);
+				picked.push_back(min_edge->weight);
+				cur = min_edge->to;
+			}
+		}
+		res = picked;
+		return picked;
+	}
+
+	void dfs(Graph *graph, Vertex *cur, unordered_set<Edge*> &memo) {
+		if (picked.size() == graph->edges.size() + 1) {
+			minDictJourey();
+			return;
+		}
+		for(auto edge : cur->edges){
+			// if the edge is not visited that show the ticket is not used
 			if (memo.find(edge) == memo.end()) {
 				memo.insert(edge);
 				picked.push_back(edge->to->val);
-				dfs(graph, edge->to, count + 1);
+				dfs(graph, edge->to, memo);
 				picked.pop_back();
+				memo.erase(edge);
 			}
 		}
 	}
 
 	void dfs(Graph *graph, string start) {
-		for (auto str_ver : graph->vertex_map) {
-			if (str_ver.first == start) {
-				picked.push_back(start);
-				dfs(graph, str_ver.second, 1);
-			}
-		}
+		unordered_set<Edge*> memo;
+		picked.push_back(start);
+		dfs(graph, graph->vertex_map[start]);
 	}
 
 	vector<string> findItinerary(vector<vector<string>>& tickets) {
 		// build graph
 		Graph *graph = generateGraph(tickets);
-		DFS(graph, "JFK");
+		//DFS(graph, "JFK");
 		// start point
 		// dfs		
 		dfs(graph, "JFK");
